94a24cd1221e6566bd05ca59b79048874b615c91
JuiceJ <joosjae01@gmail.com>
2026-08-28 10:32:13 +0900

    [Feat] 후라이드 치킨 클래스 생성

---
+++ 260828_Regular_Test/260828_Regular_Test/Items/FriedChicken.cs
@@ -0,0 +1,11 @@
+﻿public class FriedChicken : Item, ISaleable
+{
+    public FriedChicken() : base("후라이드 치킨", 13000, ItemType.Chicken) {
+
+    }
+
+    public int ApplySale(float SaleRate)
+    {
+        return (int)(BasePrice * SaleRate);
+    }
+}
