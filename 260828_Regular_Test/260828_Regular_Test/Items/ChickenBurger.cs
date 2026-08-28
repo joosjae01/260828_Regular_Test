37b1d51c192265f597c03370fecf94a5d244846c
JuiceJ <joosjae01@gmail.com>
2026-08-28 10:45:07 +0900

    [Feat] 치킨 버거에 가격 변환 및 세일 연산 함수 구현

--- 260828_Regular_Test/260828_Regular_Test/Items/ChickenBurger.cs
+++ 260828_Regular_Test/260828_Regular_Test/Items/ChickenBurger.cs
@@ -1,10 +1,20 @@
 ﻿public class ChickenBurger : Item, ISaleable
 {
     private const float SALE_RATE = 0.8f;
+    private bool _isSale = false;
     public ChickenBurger() : base("치킨 샌드위치", 5500, ItemType.Hamburger) {
 
     }
 
+    public override int GetPrice()
+    {
+        if (_isSale)
+        {
+            return ApplySale();
+        }
+
+        return BasePrice;
+    }
     public int ApplySale()
     {
         return (int)(BasePrice * SALE_RATE);
