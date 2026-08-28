ba3c8ce161ec9cb9acacb2fc3e81c8e65048f49a
JuiceJ <joosjae01@gmail.com>
2026-08-28 10:10:33 +0900

    [Feat] 추상 최상위 클래스 Item 생성

---
+++ 260828_Regular_Test/260828_Regular_Test/Item.cs
@@ -0,0 +1,11 @@
+﻿public abstract class Item
+{
+    public int BasePrice {  get; set; }
+
+    public Item(int price)
+    {
+        BasePrice = price;
+    }
+
+    public abstract void CalculatePrice();
+}
