bf31ec8cefa81a0e2d1abe481e295241737d95dd
JuiceJ <joosjae01@gmail.com>
2026-08-28 18:26:09 +0900

    [Feat] combo(세트 메뉴) 기본 구상 구현

--- 260828_Regular_Test/260828_Regular_Test/ItemBase/SideMenuBase.cs
+++ 260828_Regular_Test/260828_Regular_Test/ItemBase/SideMenuBase.cs
@@ -1,7 +1,18 @@
 ﻿public class SideMenuBase : ItemBase
 {
+    private int _comboCount = 0;
     public SideMenuBase(string name, int price) : base(name, price, ItemType.SideMenus) {
 
+    }
+
+    public void InitComboCount()
+    {
+        _comboCount = 0;
+    }
+
+    public void IncreaseComboCount()
+    {
+        _comboCount++;
     }
 
     public override int GetPrice()
