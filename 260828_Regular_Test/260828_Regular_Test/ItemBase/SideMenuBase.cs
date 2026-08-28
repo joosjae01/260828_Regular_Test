af3cfd21296103608db6f75e6a8dc88fa69d22d2
JuiceJ <joosjae01@gmail.com>
2026-08-28 18:32:41 +0900

    [Feat] 사이드 메뉴 세일율 구상

--- 260828_Regular_Test/260828_Regular_Test/ItemBase/SideMenuBase.cs
+++ 260828_Regular_Test/260828_Regular_Test/ItemBase/SideMenuBase.cs
@@ -1,6 +1,7 @@
-﻿public class SideMenuBase : ItemBase
+﻿public class SideMenuBase : ItemBase, ISaleable
 {
     private int _comboCount = 0;
+    private const float SALE_RATE = 0.7f;
     public SideMenuBase(string name, int price) : base(name, price, ItemType.SideMenus) {
 
     }
@@ -23,6 +24,11 @@
     public override void PrintItem()
     {
         base.PrintItem();
-        Console.WriteLine("[정가]");
+        Console.WriteLine("[세트 메뉴 주문시 30% 할인]");
+    }
+
+    public int ApplySale()
+    {
+        return (int)(BasePrice * SALE_RATE);
     }
 }
