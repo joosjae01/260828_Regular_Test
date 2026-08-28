d31c7884b9f944cf36d928d785e1292bbee91005
JuiceJ <joosjae01@gmail.com>
2026-08-28 10:47:06 +0900

    [Feat] 소고기 버거에 세일 함수 구현

--- 260828_Regular_Test/260828_Regular_Test/Items/BeefBurger.cs
+++ 260828_Regular_Test/260828_Regular_Test/Items/BeefBurger.cs
@@ -1,6 +1,23 @@
-﻿public class BeefBurger : Item
+﻿public class BeefBurger : Item, ISaleable
 {
+    private const float SALE_RATE = 0.7f;
+    private bool _isSale = false;
+
     public BeefBurger() : base("소고기 버거", 5900, ItemType.Hamburger) {
 
+    }
+
+    public override int GetPrice()
+    {
+        if (_isSale)
+        {
+            return ApplySale();
+        }
+
+        return BasePrice;
+    }
+    public int ApplySale()
+    {
+        return (int)(BasePrice * SALE_RATE);
     }
 }
