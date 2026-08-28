cea43e8fb04a08214291c83c0ba3346cc5be4eeb
JuiceJ <joosjae01@gmail.com>
2026-08-28 12:30:57 +0900

    [Test] 메인 함수 출력 테스트

--- 260828_Regular_Test/260828_Regular_Test/Program.cs
+++ 260828_Regular_Test/260828_Regular_Test/Program.cs
@@ -15,7 +15,7 @@ class Program
         SpiceChicken spiceChicken = new SpiceChicken();
 
         Kiosk kiosk = new Kiosk();
-        User user = new User(10000);
+        User user = new User(100000);
         // === 치킨류 ===
         kiosk.AddItem(friedChicken);
         kiosk.AddItem(spiceChicken);
@@ -27,7 +27,9 @@ class Program
         // === 사이드 ===
         kiosk.AddItem(frenchFries);
 
-        while (true)
+        bool isWorking = true;
+
+        while (isWorking)
         {
             Console.Clear();
             Console.WriteLine("----------------------------------------");
@@ -38,9 +40,31 @@ class Program
             kiosk.PrintBasket(user);
             Console.WriteLine("----------------------------------------");
             Console.WriteLine("1. 담기  2. 전체 비우기  3. 결제  4. 영업 종료");
-            int picked = ConsoleInput.ReadIntInRange("번호 : ", 1, MENU_SIZE);
+            int picked = ConsoleInput.ReadIntInRange("번호 : ", 1, 4);
 
             // 골라진 번호대로 처리하고 결과를 출력한다
+            switch (picked)
+            {
+                case 1:
+                    int menuNumber = ConsoleInput.ReadIntInRange("메뉴 번호 : ", 1, MENU_SIZE);
+                    int count = ConsoleInput.ReadIntAtLeast("개수 : ", 0);
+
+                    kiosk.PutItem(user, (menuNumber - 1), count);
+                    break;
+
+                case 2:
+                    kiosk.RefreshItem(user);
+                    break;
+
+                case 3:
+                    kiosk.PurchaseBasket(user);
+                    break;
+
+                case 4:
+                    kiosk.PrintTotal();
+                    isWorking = false;
+                    break;
+            }
 
             ConsoleInput.Pause();
         }
