64a179c802c0c2d8bd3f67728f83395bf123e1bf
JuiceJ <joosjae01@gmail.com>
2026-08-28 10:51:45 +0900

    [Feat] User 클래스 추가

---
+++ 260828_Regular_Test/260828_Regular_Test/User.cs
@@ -0,0 +1,30 @@
+﻿public class User
+{
+    public int TotalMoney { get; private set; }
+    private List<Item> _basket = new List<Item>();
+
+    public User(int totalMoney)
+    {
+        TotalMoney = totalMoney;
+    }
+
+    public void SetTotalMoney(int amount)
+    {
+        TotalMoney -= amount;
+    }
+
+    public void AddItem(Item item)
+    {
+        _basket.Add(item);
+    }
+
+    public Item GetItem(int index)
+    {
+        return _basket[index];
+    }
+
+    public void RefreshItem()
+    {
+        _basket.Clear();
+    }
+}
