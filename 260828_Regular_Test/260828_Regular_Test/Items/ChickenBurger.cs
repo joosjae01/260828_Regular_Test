e64abdc6239ad243b9d47648b5d1becef8a48a0d
JuiceJ <joosjae01@gmail.com>
2026-08-28 10:40:03 +0900

    [Feat] 치킨 버거 세일 구현

--- 260828_Regular_Test/260828_Regular_Test/Items/ChickenBurger.cs
+++ 260828_Regular_Test/260828_Regular_Test/Items/ChickenBurger.cs
@@ -1,6 +1,12 @@
-﻿public class ChickenBurger : Item
+﻿public class ChickenBurger : Item, ISaleable
 {
+    private const float SALE_RATE = 0.8f;
     public ChickenBurger() : base("치킨 샌드위치", 5500, ItemType.Hamburger) {
 
+    }
+
+    public int ApplySale()
+    {
+        return (int)(BasePrice * SALE_RATE);
     }
 }
