e43c072e9aceda6adb76bdf70514e1581b20aaa3
JuiceJ <joosjae01@gmail.com>
2026-08-29 11:21:14 +0900

    [Chore] 사이드 메뉴 할인 수치 변경 및 출력 되는 할인 조건 명확화

--- 260828_Regular_Test/260828_Regular_Test/ItemBase/SideMenuBase.cs
+++ 260828_Regular_Test/260828_Regular_Test/ItemBase/SideMenuBase.cs
@@ -1,7 +1,7 @@
 ﻿public class SideMenuBase : ItemBase, ISaleable
 {
     private int _comboCount = 0;
-    private const float SALE_RATE = 0.7f;
+    private const float SALE_RATE = 0.9f;
     public SideMenuBase(string name, int price) : base(name, price, ItemType.SideMenus) {
 
     }
@@ -29,7 +29,7 @@
     public override void PrintItem()
     {
         base.PrintItem();
-        Console.WriteLine("[세트 메뉴 주문시 30% 할인]");
+        Console.WriteLine("[버거 메뉴 하나 당 10% 할인]");
     }
 
     public float GetSaleRate()
